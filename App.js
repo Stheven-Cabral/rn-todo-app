9ba26654ecbe9b6c906a228667fd93306169075a
Stheven Cabral <stheven.cabral@gmail.com>
2020-08-07 16:54:21 -0700

    testing android emulator

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello WOrld!</Text>
+      <Text>Does this work on Pixel Android!</Text>
       <StatusBar style="auto" />
     </View>
   );
